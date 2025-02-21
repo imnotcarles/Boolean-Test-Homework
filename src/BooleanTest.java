15584af6dd1906553dfa58c2d55a8b1088dfa134
unknown <342859170@TDSB.ON.CA>
2025-02-21 11:33:42 -0500

    Revised code for Boolean Test Homework

--- src/BooleanTest.java
+++ src/BooleanTest.java
@@ -14,18 +14,7 @@ public class BooleanTest {
         boolean d = keyboard.nextBoolean();
         keyboard.close();
 
-        boolean gate1 = a && b;
-        boolean gate2 = c && d;
-        boolean gate3 = a && c;
-        boolean gate4 = a && d;
-        boolean gate5 = b && d;
-        boolean gate6 = b && c;
-        boolean gate7 = gate1 || gate2;
-        boolean gate8 = gate3 || gate4;
-        boolean gate9 = gate5 || gate6;
-        boolean gate10 = gate7 || gate8;
-        boolean gate11 = gate8 || gate9;
-        boolean gateFinal= gate10 || gate11;
+        boolean gateFinal = a && b || c && d || a && c || a && d || b && d || b && c;
 
         System.out.println("The output value is: " + gateFinal);
     }
